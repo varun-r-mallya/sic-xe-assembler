599b20b850c0448d64be2295be89c54a322e09cf
varun-r-mallya <varunrmallya@gmail.com>
2025-10-24 23:45:15 +0530

    add some functions of pass 2

--- src/pass_2.cpp
+++ src/pass_2.cpp
@@ -1,1 +1,79 @@
 #include "pass_2.h"
+#include "utilities.h"
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include "table_store.h"
+
+std::string pass_2::readTillTab(const std::string &data, int &index) {
+    std::string tempBuffer;
+
+    while (index < data.length() && data[index] != '\t') {
+        tempBuffer += data[index];
+        index++;
+    }
+    index++;
+    if (tempBuffer == " ") {
+        tempBuffer = "-1";
+    }
+    return tempBuffer;
+}
+
+bool pass_2::readIntermediateFile() {
+    std::string fileLine;
+    bool tempStatus;
+    int index = 0;
+    if (!getline(intermediateFile, fileLine)) {
+        return false;
+    }
+    lineNumber = utilities::string_to_decimal(readTillTab(fileLine, index));
+
+    isComment = fileLine[index] == '.';
+    if (isComment) {
+        utilities::readFirstNonWhiteSpace(fileLine, index, tempStatus, comment, true);
+        return true;
+    }
+    address = utilities::stringHexToInt(readTillTab(fileLine, index));
+    if (const std::string tempBuffer = readTillTab(fileLine, index); tempBuffer == " ") {
+        blockNumber = -1;
+    } else {
+        blockNumber = utilities::string_to_decimal(tempBuffer);
+    }
+    label = readTillTab(fileLine, index);
+    if (label == "-1") {
+        label = " ";
+    }
+    opcode = readTillTab(fileLine, index);
+    if (opcode == "BYTE") {
+        utilities::readByteOperand(fileLine, index, tempStatus, operand);
+    } else {
+        operand = readTillTab(fileLine, index);
+        if (operand == "-1") {
+            operand = " ";
+        }
+    }
+    utilities::readFirstNonWhiteSpace(fileLine, index, tempStatus, comment, true);
+    return true;
+}
+
+void pass_2::writeTextRecord(bool lastRecord) {
+}
+
+std::string pass_2::createObjectCodeFormat34() {
+}
+
+void pass_2::writeEndRecord(bool write) {
+}
+
+pass_2::pass_2(std::string filename, table_store *tables, const std::string &intermediateFileName,
+               const std::string &objectFileName, const std::string &listingFileName, const std::string &errorFileName,
+               std::string *blocksNumToName, const std::string &firstExecutableSec, int progLength) {
+}
+
+void pass_2::run_pass_2() {
+}
+
+pass_2::~pass_2() {
+}
--- src/pass_2.h
+++ src/pass_2.h
@@ -1,8 +1,48 @@
 #ifndef SIC_XE_ASSEMBLER_PASS_2_H
 #define SIC_XE_ASSEMBLER_PASS_2_H
 
-class pass_2 {
+#include <string>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include "table_store.h"
 
+class pass_2 {
+        std::string fileName;
+        table_store *tableStore;
+        std::ifstream intermediateFile;
+        std::ofstream errorFile;
+        std::ofstream objectFile;
+        std::ofstream listingFile;
+
+        bool isComment;
+        std::string label, opcode, operand, comment;
+        std::string operand1, operand2;
+
+        int lineNumber, blockNumber, address, startAddress;
+        std::string objectCode, writeData, currentRecord, modificationRecord, endRecord;
+        int program_counter, base_register_value;
+        bool nobase;
+
+        std::string *BLocksNumToName;
+        std::string firstExecutable_Sec;
+        int program_length;
+
+        static std::string readTillTab(const std::string &data, int &index);
+        bool readIntermediateFile();
+        void writeTextRecord(bool lastRecord = false);
+        std::string createObjectCodeFormat34();
+        void writeEndRecord(bool write = true);
+
+    public:
+        pass_2(std::string filename, table_store *tables, const std::string &intermediateFileName,
+               const std::string &objectFileName, const std::string &listingFileName,
+               const std::string &errorFileName, std::string *blocksNumToName,
+               const std::string &firstExecutableSec, int progLength);
+
+        void run_pass_2();
+
+        ~pass_2();
 };
 
 
